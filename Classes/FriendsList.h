c39c8c8c8311f99fdfbbbf1dcbc5ec7753a823e9
Oscar De Moya <oscardemoya@Oscar-De-Moyas-MacBook-3.local>
2011-02-14 11:16:08 -0500

    Added FriendList

---
+++ Classes/FriendsList.h
@@ -0,0 +1,17 @@
+//
+//  FriendsList.h
+//  BeerCounter
+//
+//  Created by Oscar De Moya on 2/14/11.
+//  Copyright 2011 Koombea Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface FriendsList : UITableViewController {
+@private
+    
+}
+
+@end
